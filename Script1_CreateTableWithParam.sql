b25ee172e258b5053581bf091628fe656a6fa4a9
Naytony <121373689+Naytony@users.noreply.github.com>
2023-08-24 20:40:09 +0300

    Add 3 SQL-scripts

---
+++ Script1_CreateTableWithParam.sql
@@ -0,0 +1,8 @@
+create table PERSONS (
+	name text,
+	surname text,
+	age integer,
+	phone_number text,
+	city_of_living text,
+	primary KEY (name, surname, age)
+);
---
+++ Script2_SearchNameAndSurname.sql
@@ -0,0 +1,14 @@
+insert into persons (name, surname, age, phone_number, city_of_living) 
+values 
+('Bob', 'Lame', 34, '+19877898888', 'BOSTON'),
+('Denis', 'Kronov', 22, '+71345798854', 'MOSCOW'),
+('Ivan', 'Dronov', 44, '+75654677742', 'MOSCOW'),
+('Mike', 'Vane', 32, '+59999678666', 'LONDON'),
+('Ron', 'Jole', 48, '+71345798854', 'MOSCOW'),
+('Katar', 'Matar', 29, '+28767688868', 'ROME'),
+('Grigoriy', 'Kinov', 19, '+71345798854', 'MOSCOW')
+;
+
+select name, surname from persons
+where city_of_living='MOSCOW';
+
